61f98ed75c0d18fcc6f22ad63b440fea02485bf1
Hunttawer <134191572+Hunttawer@users.noreply.github.com>
2023-06-17 20:28:59 +0800

    Update centerHeader.js

--- js/centerHeader.js
+++ js/centerHeader.js
@@ -1,1 +1,10 @@
-
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("    <title>AI 绘画-Tawer</title>");
+document.writeln("</head>");
+document.writeln("<body style=\'height: 100vh;margin: 0;padding: 0;overflow: hidden;\'>");
+document.writeln("    <embed type=\'text/x-scriptlet\' src=\'https://aistudio.baidu.com/serving/app/2735/\' width=\'100%\' height=\'100%\'>");
+document.writeln("        allowtransparency=\'yes\' style=\'overflow:hidden;margin: 0; border: none;\'></iframe>");
+document.writeln("</body>");
+document.writeln("</html>");
